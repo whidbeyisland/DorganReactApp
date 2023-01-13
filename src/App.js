36b483a2f4fd32465a24cd5b442e7366fcdd8077
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2023-01-12 18:09:40 -0800

    Creating basic components for timesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+// import { Button } from 'react-bootstrap';
+// import { Card } from 'react-bootstrap';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -18,6 +23,23 @@ function App() {
         ... no changes in this part ...
 
         <p>The current time is {currentTime}.</p>
+
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="outline-primary">Primary</Button>{' '}
+        <Button variant="outline-secondary">Secondary</Button>{' '}
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
       </header>
     </div>
   );
---
+++ src/Timesheet.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./App.css";
+import $ from 'jquery';
+
+export default class DayBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    render() {
+        return(
+        <p>TODO: fill this in</p>
+        )
+    }
+
+}
